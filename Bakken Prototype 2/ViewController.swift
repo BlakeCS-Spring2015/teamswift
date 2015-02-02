ab8b6f38f1c05108ee172b07c56683c5dde8a7e6
Ben Weinshel <weinshel@users.noreply.github.com>
2015-02-02 10:42:37 -0600

    Initial Commit

---
+++ Bakken Prototype 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bakken Prototype 2
+//
+//  Created by Ben Weinshel on 2/2/15.
+//  Copyright (c) 2015 Blake School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
